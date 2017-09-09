b83d89adcb6e8780123891eee2eb838bf6c37d82
Bruno Oliveira <brunotc@gmail.com>
2017-09-09 19:26:33 -0400

    Perspective correct texture.

--- geo3d.lua
+++ geo3d.lua
@@ -1,7 +1,9 @@
+-- Common formats:
 -- Mat4: 16 floats in row major format
 -- Vec4: x,y,z,w
 -- Vec3: x,y,z
-
+-- Material ID: an int. If positive, it's a color,
+--   if negative, it's a texture ID.
 -- Geom:
 --   opos: array of Vec3 with object space
 --         vertex coords
@@ -17,7 +19,7 @@
 local SCRH=136
 local SCRW=240
 
-G={
+local G={
  CULL=false,
  tr={0,0,-150},
  modelMat=nil,
@@ -60,16 +62,23 @@ function TIC()
  UpdateModelMat()
 end
 
-----------------------------------------
+-------------------------------------------------
 -- "Q", OUR 3D LIBRARY
-----------------------------------------
-Q={
+-- Q* functions are the public API.
+-- _Q* functions are private.
+-------------------------------------------------
+local Q={
  projMat=nil,
  nearClip=0,
  farClip=0,
  viewMat=nil,
  pvMat=nil,
 }
+
+-- Aliases to math functions:
+local floor,ceil,abs=math.floor,math.ceil,math.abs
+local sin,cos,tan=math.sin,math.cos,math.tan
+local PI,HUGE=math.pi,math.huge
 
 -- Init 3d library.
 -- fovy: field of view angle, degrees
@@ -123,79 +132,83 @@ function QGeoRend(geom,mat)
  end
  -- Rasterize all the tris
  for i=1,#geom.tris do
-  QTriRast(geom,geom.tris[i])
+  _QTriRast(geom,geom.tris[i])
  end
 end
 
--- Calculates order of the given values.
-function QCalcOrd(v1,v2,v3,result)
+-- Calculates descending order of the given values.
+-- Example: if v1=10, v2=3, v3=72, returns
+-- {3,1,2} because v3 is biggest, followed by v1,
+-- then v2.
+function _QCalcOrd(v1,v2,v3,result)
  if v1>=v2 and v1>=v3 then
   -- v1 is top.
   result[1]=1
-  if v2>=v3 then
-   result[2]=2 result[3]=3
-  else
-   result[2]=3 result[3]=2
-  end
+  if v2>=v3 then result[2]=2 result[3]=3
+  else result[2]=3 result[3]=2 end
  elseif v2>=v1 and v2>=v3 then
   -- v2 is top.
   result[1]=2
-  if v1>=v3 then
-   result[2]=1 result[3]=3
-  else 
-   result[2]=3 result[3]=1
-  end
+  if v1>=v3 then result[2]=1 result[3]=3
+  else result[2]=3 result[3]=1 end
  else
   -- v3 is top
   result[1]=3
-  if v1>=v2 then
-   result[2]=1 result[3]=2
-  else
-   result[2]=2 result[3]=1
-  end
+  if v1>=v2 then result[2]=1 result[3]=2
+  else result[2]=2 result[3]=1 end
  end
 end
 
--- Raster tri.
-local QTriRast_tmp={0,0,0}
-function QTriRast(geom,tri)
+-- Rast (render) a given triangle.
+-- geom: the geomtry to render. Assumes geom.spos[]
+--   has already been calculated (screen positions).
+-- tri: the triangle to rasterize.
+local _QTriRast_tmp={0,0,0}
+function _QTriRast(geom,tri)
  local p1=geom.spos[tri.v[1]]
  local p2=geom.spos[tri.v[2]]
  local p3=geom.spos[tri.v[3]]
- if Q.CULL and QTriWind(p1,p2,p3)<0
+ -- Cull back faces.
+ if Q.CULL and _QTriWind(p1,p2,p3)<0
    then return end
- local yord=QTriRast_tmp
- QCalcOrd(p1[2],p2[2],p3[2],yord)
+ -- Find order of vertices to split rasterization
+ -- into two flat-base triangles.
+ local yord=_QTriRast_tmp
+ _QCalcOrd(p1[2],p2[2],p3[2],yord)
  local top=geom.spos[tri.v[yord[1]]]
  local mid=geom.spos[tri.v[yord[2]]]
  local bot=geom.spos[tri.v[yord[3]]]
+ -- Tex coords of each vertex:
  local ttop=tri.texc[yord[1]]
  local tmid=tri.texc[yord[2]]
  local tbot=tri.texc[yord[3]]
 
  if top[2]==bot[2] then return end
  if top[2]~=mid[2] then
-  QTriRastFlat(top,ttop,mid,tmid,
-    bot,tbot,tri.mtl)
+  -- Render top part (top to middle).
+  _QTriRastFlat(top,ttop,mid,tmid,bot,tbot,tri.mtl)
  end
  if bot[2]~=mid[2] then
-  QTriRastFlat(bot,tbot,mid,tmid,
-    top,ttop,tri.mtl)
+  -- Render bottom part (bottom to middle).
+  _QTriRastFlat(bot,tbot,mid,tmid,top,ttop,tri.mtl)
  end
 end
 
-local QTriWind_tmp={0,0,0}
-local QTriWind_tmp2={0,0,0}
-function QTriWind(p1,p2,p3)
- local a=QTriWind_tmp
- local b=QTriWind_tmp2
+-- Calculate winding of given points (in screen
+-- coords). Returns positive for counter-clockwise,
+-- negative for clockwise.
+local _QTriWind_tmp={0,0,0}
+local _QTriWind_tmp2={0,0,0}
+function _QTriWind(p1,p2,p3)
+ local a=_QTriWind_tmp
+ local b=_QTriWind_tmp2
  Q3AddS(a,p2,-1,p1)
  Q3AddS(b,p3,-1,p1)
  Q3Cross(a,a,b)
  return a[3]
 end
 
--- rasterize flat-top or flat-bottom tri
+-- Rasterize flat-base triangle.
 -- a: apex (start) vertex pos
 -- ta: tex coords of apex vertex
 -- b: target (end) vertex pos
@@ -203,12 +216,17 @@ end
 -- c: other vertex pos
 -- tc: tex coords of other vertex.
 -- mtl: material ID
-function QTriRastFlat(a,ta,b,tb,c,tc,mtl)
+function _QTriRastFlat(a,ta,b,tb,c,tc,mtl)
+ -- abort if y is out of bounds
+ if a[2]<0 and b[2]<0 then return end
+ if a[2]>SCRH and b[2]>SCRH then return end
+
  local aby=a[2]-b[2]
  local acy=a[2]-c[2]
- -- derivatives of each variable (x,z,
- -- u,v) with respect to changes in y,
- -- for each segment (1=a-b and 2=a-c)
+ -- derivatives of each variable (x,z,u,v) with
+ -- respect to changes in y, for each segment.
+ -- (we say the "a-b" segment is "side 1", and the
+ -- "a-c" segment is "side 2").
  local dx1=(a[1]-b[1])/aby
  local dz1=(a[3]-b[3])/aby
  local du1=(ta[1]-tb[1])/aby
@@ -218,16 +236,15 @@ function QTriRastFlat(a,ta,b,tb,c,tc,mtl)
  local du2=(ta[1]-tc[1])/acy
  local dv2=(ta[2]-tc[2])/acy
 
- -- abort if y is out of bounds
- if a[2]<0 and b[2]<0 then return end
- if a[2]>SCRH and b[2]>SCRH then return end
-
  local ys=QClamp(a[2],0,SCRH-1) -- start y
  local yf=QClamp(b[2],0,SCRH-1) -- end y
  if not QIsFinite(ys) or not QIsFinite(yf)
    then return end
  
  local totdy=yf-a[2]
+ -- xf1 is the final x coord of side 1
+ -- xf2 is the final x coord of side 2
+ -- (etc)
  local xf1=a[1]+dx1*totdy
  local xf2=a[1]+dx2*totdy
  local zf1=a[3]+dz1*totdy
@@ -245,16 +262,24 @@ function QTriRastFlat(a,ta,b,tb,c,tc,mtl)
    then return end
  
  local dy=yf>=ys and 1 or -1
+ -- Rasterize each scanline.
  for y=ys,yf,dy do
   local dy=y-a[2]
+  -- Compute variables for this scanline.
+  -- (remember side 1 is a-b, side 2 is a-c)
   local x1=a[1]+dy*dx1
   local x2=a[1]+dy*dx2
   local z1=a[3]+dy*dz1
   local z2=a[3]+dy*dz2
-  local u1=ta[1]+dy*du1
-  local u2=ta[1]+dy*du2
-  local v1=ta[2]+dy*dv1
-  local v2=ta[2]+dy*dv2
+  -- Texture coordinates:
+  --local u1=ta[1]+dy*du1
+  --local u2=ta[1]+dy*du2
+  --local v1=ta[2]+dy*dv1
+  --local v2=ta[2]+dy*dv2
+  local u1,v1=_QPerspTexC(a[2],a[3],ta[1],ta[2],
+    b[2],b[3],tb[1],tb[2],y)
+  local u2,v2=_QPerspTexC(a[2],a[3],ta[1],ta[2],
+    c[2],c[3],tc[1],tc[2],y)
 
   if (x1>=0 and x1<SCRW) or
      (x2>=0 and x2<SCRW) then
@@ -263,18 +288,38 @@ function QTriRastFlat(a,ta,b,tb,c,tc,mtl)
    local xinc=x2c>=x1c and 1 or -1
    for x=x1c,x2c,xinc do
     if mtl>=0 then
+     -- Plain color.
      pix(x,y,mtl)
     else
-     local u=QInterp(x1,u1,x2,u2,x)
-     local v=QInterp(x1,v1,x2,v2,x)
-     pix(x,y,QTexSamp(-mtl,u,v))
+     -- Sample texture (with perspective correction).
+     local u,v=_QPerspTexC(x1,z1,u1,v1,x2,z2,u2,v2,x)
+     pix(x,y,_QTexSamp(-mtl,u,v))
     end
    end
   end
  end
 end
 
-function QTexSamp(tid,u,v)
+-- Calculates interpolated perspective-correct texture
+-- coords.
+-- c1: x (or y) coordinate of first point.
+-- z1: z coordinate of first point.
+-- u1,v1: tex coords of first point.
+-- (same for second point).
+-- c: the coordinate to find the texture coords for
+-- (must be between c1 and c2).
+function _QPerspTexC(c1,z1,u1,v1,c2,z2,u2,v2,c)
+ local a=QInterp(c1,0,c2,1,c) 
+ local f=1/((1-a)/z1+a/z2)
+ local u=((1-a)*(u1/z1)+a*(u2/z2))*f
+ local v=((1-a)*(v1/z1)+a*(v2/z2))*f
+ return u,v
+end
+
+-- Sample texture ID tid at texture coords u,v.
+-- The texture ID is just the sprite ID where
+-- the texture begins in sprite memory.
+function _QTexSamp(tid,u,v)
  -- texture size in pixels
  -- TODO make this variable
  local SX=16
@@ -291,25 +336,14 @@ end
 -- Q: MATH UTILITY
 ----------------------------------------
 
-local floor,ceil=math.floor,math.ceil
-
-function QRound(x)
- return floor(x+0.5)
-end
-
-function QIsFinite(v)
- return v<math.huge and v>-math.huge
-end
-
+function QRound(x) return floor(x+0.5) end
+function QIsFinite(v) return v<HUGE and v>-HUGE end
 function QClamp(v,l,h)
  return (v<l and l) or (v>h and h or v)
 end
 
 function QInterp(x1,y1,x2,y2,x)
- if x2<x1 then
-  x1,x2=x2,x1
-  y1,y2=y2,y1
- end
+ if x2<x1 then x1,x2=x2,x1 y1,y2=y2,y1 end
  return x<x1 and y1 or (x>x2 and y2 or
    (y1+(y2-y1)*(x-x1)/(x2-x1)))
 end
@@ -348,8 +382,8 @@ end
 
 -- Rotation from axis angle
 function QMRot(m,axis,phi)
- local c=math.cos(phi)
- local s=math.sin(phi)
+ local c=cos(phi)
+ local s=sin(phi)
  local c1=1-c
  local x=axis[1]
  local y=axis[2]
@@ -369,7 +403,7 @@ end
 
 -- Inner product of row r of a with
 -- col c of b
-function QMSubMul(a,b,r,c)
+function _QMSubMul(a,b,r,c)
  local v=0
  for i=1,4 do
   v=v+QMGet(a,r,i)*QMGet(b,i,c)
@@ -381,26 +415,26 @@ function QMMul(dest,lhs,rhs)
  if dest==lhs or dest==rhs then
   error("QMMul: no overlap allowed.")
  end
- dest[1]=QMSubMul(lhs,rhs,1,1)
- dest[2]=QMSubMul(lhs,rhs,1,2)
- dest[3]=QMSubMul(lhs,rhs,1,3)
- dest[4]=QMSubMul(lhs,rhs,1,4)
- dest[5]=QMSubMul(lhs,rhs,2,1)
- dest[6]=QMSubMul(lhs,rhs,2,2)
- dest[7]=QMSubMul(lhs,rhs,2,3)
- dest[8]=QMSubMul(lhs,rhs,2,4)
- dest[9]=QMSubMul(lhs,rhs,3,1)
- dest[10]=QMSubMul(lhs,rhs,3,2)
- dest[11]=QMSubMul(lhs,rhs,3,3)
- dest[12]=QMSubMul(lhs,rhs,3,4)
- dest[13]=QMSubMul(lhs,rhs,4,1)
- dest[14]=QMSubMul(lhs,rhs,4,2)
- dest[15]=QMSubMul(lhs,rhs,4,3)
- dest[16]=QMSubMul(lhs,rhs,4,4)
+ dest[1]=_QMSubMul(lhs,rhs,1,1)
+ dest[2]=_QMSubMul(lhs,rhs,1,2)
+ dest[3]=_QMSubMul(lhs,rhs,1,3)
+ dest[4]=_QMSubMul(lhs,rhs,1,4)
+ dest[5]=_QMSubMul(lhs,rhs,2,1)
+ dest[6]=_QMSubMul(lhs,rhs,2,2)
+ dest[7]=_QMSubMul(lhs,rhs,2,3)
+ dest[8]=_QMSubMul(lhs,rhs,2,4)
+ dest[9]=_QMSubMul(lhs,rhs,3,1)
+ dest[10]=_QMSubMul(lhs,rhs,3,2)
+ dest[11]=_QMSubMul(lhs,rhs,3,3)
+ dest[12]=_QMSubMul(lhs,rhs,3,4)
+ dest[13]=_QMSubMul(lhs,rhs,4,1)
+ dest[14]=_QMSubMul(lhs,rhs,4,2)
+ dest[15]=_QMSubMul(lhs,rhs,4,3)
+ dest[16]=_QMSubMul(lhs,rhs,4,4)
 end
 
 -- inner prod of row r of a with v (vec4)
-function QMSubMulVec4(a,r,v)
+function _QMSubMulVec4(a,r,v)
  return v[1]*QMGet(a,r,1)+
   v[2]*QMGet(a,r,2)+
   v[3]*QMGet(a,r,3)+
@@ -416,10 +450,10 @@ function QMMulVec4(dest,a,v)
   Q4Copy(QMMulVec4_tmp,v)
   v=QMMulVec4_tmp
  end
- dest[1]=QMSubMulVec4(a,1,v)
- dest[2]=QMSubMulVec4(a,2,v)
- dest[3]=QMSubMulVec4(a,3,v)
- dest[4]=QMSubMulVec4(a,4,v)
+ dest[1]=_QMSubMulVec4(a,1,v)
+ dest[2]=_QMSubMulVec4(a,2,v)
+ dest[3]=_QMSubMulVec4(a,3,v)
+ dest[4]=_QMSubMulVec4(a,4,v)
  return dest
 end
 
@@ -435,7 +469,7 @@ function QMFrust(dest,l,r,b,t,n,f)
 end
 
 function QMPersp(dest,fovy,asp,n,f)
- local sh=n*math.tan(fovy/360*math.pi);
+ local sh=n*tan(fovy/360*PI);
  local sw=asp*sh;
  QMFrust(dest,-sw,sw,-sh,sh,n,f);
 end
