100a86e161ece3c0898da0ba775be769dafbf073
Bruno Oliveira <brunotc@gmail.com>
2017-09-14 08:08:19 -0400

    WIP

--- simpl3d.lua
+++ simpl3d.lua
@@ -1,19 +1,29 @@
 local G={
  ex=0, ey=25, ez=100, yaw=0,
+ lvlNo=0,
+ lvl=nil,  -- reference to LVL[lvlNo]
+}
+
+local LVL={
+ -- Each has:
+ --   name: display name of level.
+ --   pg: map page where level starts.
+ --   pgw,pgh: width and height of level, in pages.
+ {name="Level 1",pg=0,pgw=1,pgh=1},
 }
 
 function Boot()
  S3Init()
 
- S3WallAdd({lx=0,lz=0,rx=50,rz=0,tid=1})
- S3WallAdd({lx=50,lz=0,rx=50,rz=-50,tid=3})
- S3WallAdd({lx=50,lz=-50,rx=0,rz=-50,tid=5})
- S3WallAdd({lx=0,lz=-50,rx=0,rz=0,tid=7})
+ S3WallAdd({lx=0,lz=0,rx=50,rz=0,tid=256})
+ S3WallAdd({lx=50,lz=0,rx=50,rz=-50,tid=258})
+ S3WallAdd({lx=50,lz=-50,rx=0,rz=-50,tid=260})
+ S3WallAdd({lx=0,lz=-50,rx=0,rz=0,tid=262})
 
- S3WallAdd({lx=100,lz=0,rx=150,rz=0,tid=1})
- S3WallAdd({lx=150,lz=0,rx=150,rz=-50,tid=3})
- S3WallAdd({lx=150,lz=-50,rx=100,rz=-50,tid=5})
- S3WallAdd({lx=100,lz=-50,rx=100,rz=0,tid=7})
+ S3WallAdd({lx=100,lz=0,rx=150,rz=0,tid=256})
+ S3WallAdd({lx=150,lz=0,rx=150,rz=-50,tid=258})
+ S3WallAdd({lx=150,lz=-50,rx=100,rz=-50,tid=260})
+ S3WallAdd({lx=100,lz=-50,rx=100,rz=0,tid=262})
 end
 
 function TIC()
@@ -33,11 +43,31 @@ function TIC()
  S3Rend()
 end
 
+function StartLevel(lvlNo)
+ G.lvlNo=lvlNo
+ G.lvl=LVL[lvlNo]
+ S3Reset()
+
+ local c0,r0=MapPageStart(G.lvl.pg)
+ for r=r0,r0+
+ WIP
+end
+
+function LvlTile(c,r)
+ local c0,r0=MapPageStart(G.lvl.pg)
+ -- TODO
+end
+
+-- Returns col,row where the given map page starts.
+function MapPageStart(pg)
+ return (pg%8)*30,(pg//8)*16
+end
+
 ---------------------------------------------------
 -- S3 "Simple 3D" library
 ---------------------------------------------------
 
-local S={
+local S3={
  -- eye coordinates (world coords)
  ex=0, ey=0, ez=0, yaw=0,
  -- Precomputed from ex,ey,ez,yaw:
@@ -73,27 +103,32 @@ local SCRW=240
 local SCRH=136
 
 function S3Init()
+ S3Reset()
+end
+
+function S3Reset()
  S3SetCam(0,0,0,0)
+ S3.walls={}
 end
 
 function S3WallAdd(w)
- table.insert(S.walls,{lx=w.lx,lz=w.lz,rx=w.rx,
+ table.insert(S3.walls,{lx=w.lx,lz=w.lz,rx=w.rx,
    rz=w.rz,tid=w.tid})
 end
 
 function S3SetCam(ex,ey,ez,yaw)
- S.ex,S.ey,S.ez,S.yaw=ex,ey,ez,yaw
+ S3.ex,S3.ey,S3.ez,S3.yaw=ex,ey,ez,yaw
  -- Precompute some factors we will need often:
- S.cosMy,S.sinMy=cos(-yaw),sin(-yaw)
- S.termA=-ex*S.cosMy-ez*S.sinMy
- S.termB=ex*S.sinMy-ez*S.cosMy
+ S3.cosMy,S3.sinMy=cos(-yaw),sin(-yaw)
+ S3.termA=-ex*S3.cosMy-ez*S3.sinMy
+ S3.termB=ex*S3.sinMy-ez*S3.cosMy
 end
 
 function S3Proj(x,y,z)
- local c,s,a,b=S.cosMy,S.sinMy,S.termA,S.termB
+ local c,s,a,b=S3.cosMy,S3.sinMy,S3.termA,S3.termB
  -- Hard-coded from manual matrix calculations:
  local px=0.9815*c*x+0.9815*s*z+0.9815*a
- local py=1.7321*y-1.7321*S.ey
+ local py=1.7321*y-1.7321*S3.ey
  local pz=s*x-z*c-b-0.2
  local pw=x*s-z*c-b
  local ndcx=px/pw
@@ -103,11 +138,11 @@ end
 
 function S3Rend()
  -- TODO: compute potentially visible set instead.
- local pvs=S.walls
- local hbuf=S.hbuf
+ local pvs=S3.walls
+ local hbuf=S3.hbuf
  -- For an explanation of the rendering, see: https://docs.google.com/document/d/1do-iPbUHS2RF-lJAkPX98MsT9ZK5d5sBaJmekU1bZQU/edit#bookmark=id.7tkdwb6fk7e2
- _PrepHbuf(hbuf,pvs)
- _RendHbuf(hbuf)
+ _S3PrepHbuf(hbuf,pvs)
+ _S3RendHbuf(hbuf)
 end
 
 function _S3ResetHbuf(hbuf)
@@ -123,8 +158,8 @@ end
 
 -- Compute screen-space coords for wall.
 function _S3ProjWall(w)
- local topy=S.W_TOP_Y
- local boty=S.W_BOT_Y
+ local topy=S3.W_TOP_Y
+ local boty=S3.W_BOT_Y
 
  -- notation: lt=left top, rt=right top, etc.
  local ltx,lty,ltz=S3Proj(w.lx,topy,w.lz)
@@ -138,14 +173,14 @@ function _S3ProjWall(w)
  w.srx,w.srz,w.srty,w.srby=rtx,rtz,rty,rby
 
  -- TODO: fix aggressive clipping
- if w.slz<S.NCLIP or w.srz<S.NCLIP
+ if w.slz<S3.NCLIP or w.srz<S3.NCLIP
    then return false end
- if w.slz>S.FCLIP or w.srz>S.FCLIP
+ if w.slz>S3.FCLIP or w.srz>S3.FCLIP
    then return false end
  return true
 end
 
-function _PrepHbuf(hbuf,walls)
+function _S3PrepHbuf(hbuf,walls)
  _S3ResetHbuf(hbuf)
  for i=1,#walls do
   local w=walls[i]
@@ -178,14 +213,14 @@ function _AddWallToHbuf(hbuf,w)
  end
 end
 
-function _RendHbuf(hbuf)
+function _S3RendHbuf(hbuf)
  local scrw=SCRW
  for x=0,scrw-1 do
   local hb=hbuf[x+1]  -- hbuf is 1-indexed
   local w=hb.wall
   if w then
    local u=_S3PerspTexU(w.slx,w.slz,w.srx,w.srz,x)
-   _RendTexCol(w.tid,x,hb.ty,hb.by,u)
+   _S3RendTexCol(w.tid,x,hb.ty,hb.by,u)
   end
  end
 end
@@ -196,7 +231,7 @@ end
 --   x: x coordinate
 --   ty,by: top and bottom y coordinate.
 --   u: horizontal texture coordinate (0 to 1)
-function _RendTexCol(tid,x,ty,by,u)
+function _S3RendTexCol(tid,x,ty,by,u)
  line(x,ty,x,by,tid)
  local aty,aby=max(ty,0),min(by,SCRH-1)
  for y=aty,aby do
