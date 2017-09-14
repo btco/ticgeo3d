59335254572551ee8b2dbe5e1b56e631ebf42d81
Bruno Oliveira <btco@google.com>
2017-09-14 19:57:45 -0400

    Mac support for script.

--- tic80sync.sh
+++ tic80sync.sh
@@ -4,11 +4,16 @@ set -e
 trap "echo '*** ERROR.'; exit 1" ERR
 
 L=geo3d.tic
+STAT_CMD="stat -c %Y"
 
 if [ -d /sdcard ]; then
   echo "Auto-detected platform: Android."
   R=/sdcard/Android/data/com.nesbox.tic/files/TIC-80/geo3d.tic
   echo "TIC-80 file: $R"
+elif [ -d /Library ]; then
+  echo "Auto-detected platform: MacOSX."
+  R="$HOME/Library/Application Support/com.nesbox.tic/TIC-80/geo3d.tic"
+  STAT_CMD="stat -f %m"
 elif [ -d /c/Users ]; then
   echo "Auto-detected platform: Windows"
   R=/c/Users/bruno/AppData/Roaming/com.nesbox.tic/TIC-80/geo3d.tic
@@ -19,10 +24,10 @@ echo "  $L"
 echo "TIC-80 file:"
 echo "  $R"
 
-lt=$(stat -c %Y $L)
-rt=$(stat -c %Y $R)
+lt=$($STAT_CMD "$L")
+rt=$($STAT_CMD "$R")
 
-if diff -q $L $R; then
+if diff -q "$L" "$R"; then
   echo "Already in sync (contents same)."
   exit 0
 fi
@@ -30,13 +35,13 @@ fi
 if [ $lt -gt $rt ]; then
   echo "Local is newer than remote."
   echo -n "Copy LOCAL to REMOTE (y/N)? "
-  src=$L
-  dest=$R
+  src="$L"
+  dest="$R"
 elif [ $lt -lt $rt ]; then
   echo "Remote is newer than local."
   echo -n "Copy REMOTE to LOCAL (y/N)? "
-  src=$R
-  dest=$L
+  src="$R"
+  dest="$L"
 else
   echo "Already in sync (timestamps same)."
   exit 0
@@ -44,7 +49,7 @@ fi
 
 read ans
 if [ "$ans" = "y" ]; then
-  cp -vf $src $dest
+  cp -vf "$src" "$dest"
   echo "Copied."
 else
   echo "*** Aborted!"
